72f500bdd166306dec7c501fc2fb63786e2fbbc6
Mnimaya <mnimayaaa@gmail.com~>
2024-12-03 06:37:01 +0300

    fix: media

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const header = document.querySelector('header');
+
+document.querySelectorAll('.anchor-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetID = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetID);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+
+        header.classList.remove('active')
+        document.body.classList.remove('lock')
+    });
+});
+document.querySelector('.header__btn').addEventListener("click", () => {
+    document.body.classList.toggle('lock')
+    header.classList.toggle("active")
+});
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: "auto",
+    spaceBetween: 15,
+    breakpoints: {
+        320: {
+            slidesPerView: "auto",
+            spaceBetween: 15,
+        },
+        1120: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    }
+});
